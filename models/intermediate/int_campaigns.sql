52096242496b96add8403559ff6d9dc4841f46d5
sahillewagon@gmail.com <sahillewagon@gmail.com>
2024-07-31 12:33:29 +0000

    campaigns sql model

---
+++ models/intermediate/int_campaigns.sql
@@ -0,0 +1,7 @@
+SELECT * FROM {{ref("stg_raw__adwords")}} 
+UNION ALL
+SELECT * FROM {{ref("stg_raw__bing")}} 
+UNION ALL 
+SELECT * FROM {{ref("stg_raw__criteo")}} 
+UNION ALL
+SELECT * FROM {{ref("stg_raw__facebook")}}
